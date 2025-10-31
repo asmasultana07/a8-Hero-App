5cbec1f111fced7a4401eebaed17bb65379da9eb
asmasultana07 <ab.asma1084@gmail.com>
2025-10-31 23:31:33 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      
+      <header>
+        <Navbar></Navbar>
+      </header>
+      
+      
+    </>
+  )
+}
+
+export default App
